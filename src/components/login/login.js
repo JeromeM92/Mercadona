318057eb403005fea8a6c80f8ed25455084efff8
JeromeM92 <jerome.marras@hotmail.fr>
2023-10-22 18:28:06 +0200

    Add login screen ui

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,4 +1,45 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 70vh;
+`;
+const Title = styled.h1`
+  padding-bottom: 100px;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: 10px;
+`;
+const Label = styled.label`
+  padding-bottom: 10px;
+  font-size: 15px
+`;
+const Input = styled.input`
+  height: 40px;
+  border: 1px solid #239C6B;
+  background-color: #E7E7E7;
+  width: 240px;
+  margin-bottom: 30px;
+  border-radius: 10px;
+`;
+const LoginButton = styled.button`
+  border: 1px solid #239C6B;
+  background-color: #F5B51D;
+  border-radius: 20px;
+  font-size: 15px;
+  height: 40px;
+  width: 115px;
+  font-family: 'carme-regular';
+  margin-top: 20px; /* Ajoutez une marge au-dessus du bouton */
+`;
 
 class Login extends Component {
   constructor(props) {
@@ -19,33 +60,35 @@ class Login extends Component {
 
   handleLogin = () => {
     const { email, password } = this.state;
-    // Ici, vous pouvez ajouter la logique de connexion, par exemple, en appelant une API.
   }
 
   render() {
     return (
-      <div>
-        <h1>Connexion</h1>
-        <div>
-          <label htmlFor="email">E-mail :</label>
-          <input
+      <StyledDiv>
+        <Title>Connexion</Title>
+        <InputContainer>
+          <Label htmlFor="email">E-mail :</Label>
+          <Input
             type="email"
             id="email"
             value={this.state.email}
-            onChange={this.handleEmailChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Mot de passe :</label>
-          <input
+            onChange={this.handleEmailChange}>
+          </Input>
+
+        </InputContainer>
+        <InputContainer>
+          <Label htmlFor="password">Mot de passe :</Label>
+          <Input
             type="password"
             id="password"
             value={this.state.password}
-            onChange={this.handlePasswordChange}
-          />
-        </div>
-        <button onClick={this.handleLogin}>Se connecter</button>
-      </div>
+            onChange={this.handlePasswordChange}>
+          </Input>
+        </InputContainer>
+        <LoginButton onClick={this.handleLogin}>Connexion</LoginButton>
+      </StyledDiv>
+
+
     );
   }
 }
